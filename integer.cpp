5e320ae4105ba53f3a6f98ec74012e0fc66da9bb
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-09-28 10:34:58 +0530

    Create integer.cpp

---
+++ integer.cpp
@@ -0,0 +1,28 @@
+//Maitreyee Gohad
+//24070123509
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a;
+    cout<<"Enter  number:\n";
+    cin>>a;
+    if(a>=0)
+        {
+            cout<<"Positive number";
+        }
+    else
+        {
+            cout<<"Negative number";
+        }
+}
+
+/*OUTPUT:
+
+Enter a number: 5
+Positive number.
+
+Enter a number: -6
+Negative number.*/
