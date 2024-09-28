dbaeae8618a54fe84bb6a291f30a8e23f3602d76
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-09-28 10:34:13 +0530

    Create grade.cpp

---
+++ grade.cpp
@@ -0,0 +1,23 @@
+//Maitreyee Gohad
+//24070123509
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int s1, s2, s3, s4, s5, t;
+    float p;
+    cout<<"Enter marks of five subjects:"<<endl;
+    cin>>s1>>s2>>s3>>s4>>s5;
+    t=s1+s2+s3+s4+s5;
+    p=t/5;
+    cout<<"Percentage is "<<p<<endl;
+
+}
+
+
+/*OUTPUT:
+
+Enter marks of five sujects:
+90 89 45 30 77
+Percentage is 66.2*/
